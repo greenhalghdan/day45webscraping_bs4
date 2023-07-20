1a6925f0f1e78e5afbf832a1db18409d6259366e
danie <greenhalghdan@gmail.com>
2023-07-20 23:01:36 +0100

    scraping a live website

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 
 from bs4 import BeautifulSoup
+import requests
+# live site https://news.ycombinator.com/
 
-with open("website.html") as data:
-    contents = data.read()
+response = requests.get("https://news.ycombinator.com/")
 
-soup = BeautifulSoup(contents, "html.parser")
-#print(soup.title.string)
-# print(soup.prettify())
-print(soup.a)
-t = soup.findAll(name="a")
-print(t)
-l = soup.find(name="h1", id="name")
-print(l)
-HTMLclass = soup.find(name="h3", class_="heading")
-print(HTMLclass.getText())
-# for tag in t:
-#     #print(tag.getText())
-#     # print(tag.get("href"))
-#     print(tag)
+web_page = response.text
+soup = BeautifulSoup(web_page, "html.parser")
+span = soup.find_all(class_="titleline")
+article_text = []
+article_link = []
+for article in span:
+    article_text.append(article.find("a").getText())
+    article_link.append(article.find("a").get("href"))
+article_score = [int(score.getText().split()[0]) for score in soup.find_all(class_="score")]
+
+
+
+print(article_text)
+print(article_link)
+print(article_score)
+most_votes = article_score.index(max(article_score))
+print(article_text[most_votes])
+print(article_link[most_votes])
+print(article_score[most_votes])
+print
+
+
+
+# local site
+
+
+# BeautifulSoupwith open("website.html") as data:
+#     contents = data.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# #print(soup.title.string)
+# # print(soup.prettify())
+# print(soup.a)
+# t = soup.findAll(name="a")
+# print(t)
+# l = soup.find(name="h1", id="name")
+# print(l)
+# HTMLclass = soup.find(name="h3", class_="heading")
+# print(HTMLclass.getText())
+# # for tag in t:
+# #     #print(tag.getText())
+# #     # print(tag.get("href"))
+# #     print(tag)
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
